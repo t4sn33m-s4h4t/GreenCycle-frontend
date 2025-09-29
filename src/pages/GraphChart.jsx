2bd09b8341d70ad798e7d04891d4f40a0e48a61c
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2025-09-29 23:03:13 +0600

    fixed hodeploy

--- src/pages/GraphChart.jsx
+++ src/pages/GraphChart.jsx
@@ -45,6 +45,7 @@ const Charts = () => {
     }
 
     try {
+      console.log(import.meta.env.VITE_BACKEND_URL)
       setLoading(true);
       const res = await axios.get(
         `${import.meta.env.VITE_BACKEND_URL}/api/weather`,
